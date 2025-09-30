bfe3a2ad075f443ca0d9c2b3ef1f111034fa70ed
Liz Herrera <estiuned@gmail.com>
2025-09-30 17:30:37 -0400

    code updated
    renamed some lines for early-data MVPA analysis

--- convert_fir_data_for_MVPA_earlylate.m
+++ convert_fir_data_for_MVPA_earlylate.m
@@ -1,5 +1,5 @@
 %% this script takes "indiv_timecourses" for the COSPAL_timecourses study...
-%% and builds a dataframe for machine learning with plMVPA_Lite
+%% and builds a dataframe for machine learning with plMVPA_Lite script
 % plMVPA_Lite (and other machine learning code) typically assume all the
 % data are in ONE matrix, and there is a separate header row or file which
 % labels the conditions.
@@ -10,13 +10,13 @@
 
 
 %% first, load the "indiv_timecourses" file from Liz's code (Linux Workstation)
-load("indiv_timecourses.mat")
+load("indiv_timecourses_early-late.mat")
 
 %% now create a single matrix per task condition: NEW OL and NOL: early-late
-% old (from convert_fir_data_for_MVPA script): (OL_new, NOL_new, OL_old, NOL_old)
+% (OL_new_early, NOL_new_early, OL_new_late, NOL_new_late)
 
 % this code will "unpack" the cell array into a matrix. With the current
-% indiv_timecourses.mat data structure this results in a matrix in the
+% indiv_timecourses_early-late.mat data structure this results in a matrix in the
 % correct format for plMVPA_Lite: rows = timecourse features, columns =
 % [sub1_roi1, sub1_roi2... sub1_roi6, sub2_roi1, sub2_roi2...
 % ...sub20_roi6]
@@ -25,22 +25,21 @@ load("indiv_timecourses.mat")
 % your design (e.g., splitting the task out by early and late learning
 % would double the number of conditions)
 
-%in this section, the variables (e.g., indiv_mean_new_ol) are referring to
-%the variables loaded in from the "indiv_timecourses.mat" file (or, the
-%"early-late" version of that file)
+%in this section, the variables (e.g., indiv_mean_new_ol_early) are referring to
+%the variables loaded in from the "indiv_timecourses_early-late.mat" file
 
 Tmp=cat(2,indiv_mean_new_ol_early{:})'; %get new_ol_early matrix
-temp_olnew_mat=cell2mat(Tmp.');
+temp_ol_new_early_mat=cell2mat(Tmp.');
 
 Tmp=cat(2,indiv_mean_new_ol_late{:})'; %get new_ol_late matrix
-temp_olnew_mat=cell2mat(Tmp.');
+temp_ol_new_late_mat=cell2mat(Tmp.');
 
 Tmp=cat(2,indiv_mean_new_nol_early{:})'; %get new_nol_early matrix
-temp_olnew_mat=cell2mat(Tmp.');
+temp_nol_new_early_mat=cell2mat(Tmp.');
 
 Tmp=cat(2,indiv_mean_new_nol_late{:})'; %get new_nol_late matrix
-temp_olnew_mat=cell2mat(Tmp.');
-
+temp_nol_new_late_mat=cell2mat(Tmp.');
+% OLD
 % Tmp=cat(2,indiv_mean_new_ol{:})'; %get new_ol_new matrix
 % temp_olnew_mat=cell2mat(Tmp.');
 % 
@@ -58,18 +57,24 @@ temp_olnew_mat=cell2mat(Tmp.');
 % plMVPA_lite to use to indext the patterns for training and testing
 
 % first have code count how many ROIs are in the dataset, then generate 
-% condition names that correspond to them (e.g., one subject will have 6 
-% ol_new "conditions" in the timecourse dataframe we could analyze - one 
-% for each ROI)
+% condition names that correspond to them (e.g., one subject will have 12 "conditions" 
+% in the timecourse dataframe we could analyze:
+% Six ol_new_early "conditions" and 6 ol_new_late "conditions"-(two for each ROI)
+
+% roi1 = [bilat_RSComplex];
+% roi2 = [bilat_ba2930];
+% roi3 = [bilat_hippo];
+% roi4 = [bilat_NAcc];
+% roi5 = [bilat_caudate];
+% roi6 = [bilat_putamen];
 
 % Add or remove lines depending on the number of experiment conditions in 
 % your design (e.g., splitting the task out by early and late learning
-% would double the number of conditions)
+% would double the number of conditions, i.e., 12 conditions per ROI instead of 6)
 
-for i = 1:length(indiv_mean_new_ol{1}) %we arbitrarily look at the first \\
+for i = 1:length(indiv_mean_new_ol_early{1}) %we arbitrarily look at the first \\
     % subject {1} and count how many tcs there are (which = # of ROIs)
-    % <-this variable name is again from indiv_timecourses.mat (or
-    % early-late)
+    % <-this variable name is again from indiv_timecourses_early-late.mat 
 
 temp_ol_new_early_names{i} = ['ol_new_early_roi' num2str(i)];
 temp_ol_new_late_names{i} = ['ol_new_late_roi' num2str(i)];
@@ -87,7 +92,7 @@ end
 % names list that will label ALL the patterns for the number of subjects in 
 % our dataframe
 
-s = length(indiv_mean_new_ol); % multiplier that counts number of subjects temp_ol_new_early_names
+s = length(indiv_mean_new_ol_early); % multiplier that counts number of subjects temp_ol_new_early_names
 ol_new_early_names_allsubs = repmat(temp_ol_new_early_names, 1, s);
 ol_new_late_names_allsubs = repmat(temp_ol_new_late_names, 1, s);
 nol_new_early_names_allsubs = repmat(temp_nol_new_early_names, 1, s);
@@ -100,7 +105,8 @@ nol_new_late_names_allsubs = repmat(temp_nol_new_late_names, 1, s);
 
 
 % ok - now, let's concatenate the patterns into one massive dataframe
-testmat = [temp_ol_new_early_names_mat temp_ol_new_late_names_mat temp_nol_new_early_names_mat temp_nol_new_late_names_mat];
+% patterns from the matrix section above
+testmat = [temp_ol_new_early_mat temp_ol_new_late_mat temp_nol_new_early_mat temp_nol_new_late_mat];
 %testmat = [temp_olnew_mat temp_nolnew_mat temp_olold_mat temp_nolold_mat];
 
 % the order of the conditions (ol_new, nol_old, etc) doesn't matter, BUT 
@@ -125,8 +131,8 @@ end
 
 
 %% save our work for analysis in plMVPA_Lite
-save('FeatSet_01.mat','testmat') % let's call this dataframe feature set \\
-% #1, since we will likely do the same thing with the "early vs late" 
+save('FeatSet_02.mat','testmat') % let's call this dataframe feature set \\
+% #2 for the "early vs late" 
 % analyses and that will be a new dataframe or "feature set" for MVPA
-save('FeatSet_01_namesfile','names','onsets') %save our "model file" for \\
+save('FeatSet_02_namesfile','names','onsets') %save our "model file" for \\
 % plMVPA_Lite with the corresponding name to its feature set
